c9294acb8f4e63d638be4bd8d88350eb75be0872
JauniKappauni <social.jauni@gmail.com>
2026-01-28 16:09:31 +0100

    feat: add execution of command

--- src/main/java/de/jauni/axcalendar/listener/InventoryClickListener.java
+++ src/main/java/de/jauni/axcalendar/listener/InventoryClickListener.java
@@ -1,9 +1,11 @@
 package de.jauni.axcalendar.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryClickListener implements Listener {
 
@@ -11,6 +13,14 @@ public class InventoryClickListener implements Listener {
     public void onInventoryClick(InventoryClickEvent e) {
         if (e.getView().getTitle().equals("Calendar")) {
             e.setCancelled(true);
+            if(e.getCurrentItem() != null){
+                Player p = (Player) e.getWhoClicked();
+                ItemStack reward = e.getInventory().getItem(0);
+                if(reward != null){
+                    String cmd = "plugins";
+                    p.performCommand(cmd);
+                }
+            }
         }
     }
 }
