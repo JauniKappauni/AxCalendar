245342c7ba7fe3baef84627be5dd0e852987e7e2
JauniKappauni <social.jauni@gmail.com>
2026-01-28 09:41:12 +0100

    init: initialized project with basic structure

---
+++ src/main/java/de/jauni/axcalendar/AxCalendar.java
@@ -0,0 +1,17 @@
+package de.jauni.axcalendar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxCalendar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
